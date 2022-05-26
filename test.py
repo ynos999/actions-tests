a83a4accc1d42dcda653a78c8d2c4618fe0b4137
Ed <60920538+ynos999@users.noreply.github.com>
2022-05-26 21:41:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,9 +3,10 @@ import os
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from datetime import datetime, timedelta
 
-laiks = datetime.strftime(datetime.now() + timedelta(1), '%Y%m%d') 
-laiksMape = datetime.strftime(datetime.now() + timedelta(1), '%Y-%m-%d')
+laiks = datetime.strftime(datetime.now() + timedelta(1), '%Y%m%d') # PDF faila laika nosaukuma formāts 20220505 šodien + 1 diena.
+laiksMape = datetime.strftime(datetime.now() + timedelta(1), '%Y-%m-%d') # Mapes laika nosaukuma formāts 2022-05-05 šodien + 1 diena.
 
+# Split PDF - PDF failu sagriež.
 def pdf_splitter(path):
     fname = os.path.splitext(os.path.basename(path))[0]
 
@@ -20,16 +21,16 @@ def pdf_splitter(path):
         if pageNumber >8:
             fileprefix = str(pageNumber+1)
 
-        output_filename = "/var/www/html/" + str(laiksMape) + "/{}_{}_{}.pdf".format(
-            fname, laiks, fileprefix)
+        output_filename = "/var/files/DIENA/" + str(laiksMape) + "/{}_{}_{}.pdf".format(
+            fname, laiks, fileprefix) # Pieliekās faila nosaukums + laika formāts + lapas nr. pēc kārtas.
         with open(output_filename, 'wb') as out:
             pdf_writer.write(out)
 
         print('Created: {}'.format(output_filename))
 
-path ="/var/www/html/" + str(laiksMape) + "/DIENA.pdf"
+path ="/var/files/DIENA/" + str(laiksMape) + "/DIENA.pdf"
 pdf_splitter(path)
 
-os.remove("/var/www/html/"+ (laiksMape) +"/DIENA.pdf")
+os.remove("/var/files/DIENA/"+ (laiksMape) +"/DIENA.pdf")
 
 os.system('php /var/www/html/nextcloud/occ files:scan --all')
